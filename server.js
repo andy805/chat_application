6e61d4503b295cad10aa41adc0da5534150418cb
Andy <andyvelazquez13@yahoo.com>
2021-09-04 19:08:37 -0700

    add post request, check username and password on post request

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ import { createServer } from "http";
 import { readFileSync } from "fs";
 import {WebSocketServer} from "ws";
 import express from "express";
+import cors from 'cors';
 
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
 const port = 3000;
 const users = [{username: "andy",
               password: "1234"},
@@ -34,9 +38,21 @@ wss.on('connection', function connection(ws, req) {
 });
 
 app.get("/" , (req, res) => {
-    console.log(req);
-    console.log(res);
 
+})
+
+app.post("/" , (req, res) => {
+    console.log(req);
+    let user = req.body;
+    console.log(user);
+    for(let i = 0; i < users.length; i++) {
+        if(user.username === users[i].username && user.password === users[i].password) {
+            const validUser = { isValidUser: "1"};
+            res.json(validUser);
+            return;
+        }
+    }
+    res.status(500).json({error: "not valid"});
 })
 
 app.listen( port, () => {
