fb9f273d15a78cc92d0f898ce680b61e32fdc549
Andy <andyvelazquez13@yahoo.com>
2021-09-08 23:53:14 -0700

    add barebone signup page

---
+++ client/src/pages/Signup.js
@@ -0,0 +1,17 @@
+import React , {useState} from 'react'
+
+const Signup = () => {
+   
+    return (
+        <form>
+
+            <input type="text"></input>
+            <input type="text"></input>
+            <input type="email"></input>
+            <input type="password"></input>
+
+        </form>
+    )
+}
+
+export default Signup
